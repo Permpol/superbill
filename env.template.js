23d857e5eaa4756d5928c368a40fb94ecbcd94f8
Permpol <Permpol@users.noreply.github.com>
2025-09-21 06:30:41 +0000

    deploy: 0e8785795ca3e564e19ee86b9abcbfce6e487d76

--- env.template.js
+++ env.template.js
@@ -1,10 +1,10 @@
 // env.template.js
 const CONFIG = {
+    // สำหรับ CI ให้ใช้ GitHub Secrets -> workflow จะแทนค่าและสร้าง env.js ขณะ deploy
     CLIENT_ID: '${{ secrets.GOOGLE_CLIENT_ID }}',
     API_KEY: '${{ secrets.GOOGLE_API_KEY }}',
     DISCOVERY_DOCS: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-    SCOPES: 'https://www.googleapis.com/auth/drive.readonly',
-    API_ENDPOINT: '${{ secrets.API_ENDPOINT }}'
+    SCOPES: 'https://www.googleapis.com/auth/drive.readonly'
 };
 
 export default CONFIG;
